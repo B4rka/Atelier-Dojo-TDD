20019304180a36aa6892c5f3e87519938ed12b8a
B4rka <p.e.dejonghe@gmail.com>
2024-05-29 12:01:29 +0200

    fin du Dojo TDD

--- src/Molkky.php
+++ src/Molkky.php
@@ -6,8 +6,55 @@ class Molkky
 {
     public function getScore(array $bowlingSeries)
     {
+        $isZero = 0;
+        $pins = 0;
         $result = 0;
+        foreach($bowlingSeries as $throw) {
+                foreach($throw as $value){
+                    if ( count($throw) > 1) {
+                        $pins++;
+                    } else {
+                        if ($value === 0) {
+                            $isZero++;
+                        } else {
+                            $isZero = 0;
+                            $result += $value;
+                        }
+                    }
+                }
+            } 
+        
+        $result += $pins;
+        $result = $this->overFifty($result);
+        $result = $this->winOrLose($result, $isZero);
+
+        // var_dump($this->overFifty($result));
+        // die();
 
         return $result;
     }
+
+    public function overFifty($result) {
+        if ($result > 50) {
+            $result = 25;
+        } else {
+            $result = $result;
+        }
+        return $result;
+    }
+
+    public function winOrLose($result, $isZero)
+    {
+        if ($result === 50) {
+            $result = 'You win !';
+            return $result;
+        } elseif ($isZero === 3) {
+            $result = 'You lose !';
+            return $result;
+        }
+        return $result;
+    }
 }
+
+$molkky = new Molkky();
+echo $molkky->getScore([[0], [0], [5], [0]]);
--- tests/MolkkyTest.php
+++ tests/MolkkyTest.php
@@ -1,6 +1,39 @@
 <?php
 
-class MolkkyTest
-{
+use App\Molkky;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 
+class MolkkyTest extends TestCase
+{
+    #[DataProvider('provideData')]
+    public function testGetScore($score, $expectedScore)
+    {
+        $molkky = new Molkky;
+        
+        $this->assertSame($expectedScore, $molkky->getScore($score));
+        // $this->assertSame('You win !', $molkky->getScore([[20],[5],[20],[5]]));
+        // $this->assertEquals(49, $molkky->getScore([[19],[5],[20],[5]]));
+        // $this->assertEquals(10, $molkky->getScore([[0],[0],[10],[0]]));
+        // $this->assertEquals(5, $molkky->getScore([[0],[0],[5],[0]]));
+        // $this->assertEquals(35, $molkky->getScore([[20],[5],[5],[0]]));
+    }
+
+    public static function provideData()
+    {
+        return [
+            'First Dataset' => [
+                'score' => [[20], [0], [0], [0]],
+            'expectedScore' => 'You lose !'
+            ],
+            'Second Dataset' => [
+                'score' => [[20], [20], [10]],
+            'expectedScore' => 'You win !'
+            ],
+            'Third Dataset' => [
+                'score' => [[10], [1,3,5], [4,3], [8,7]],
+            'expectedScore' => 17
+            ]
+        ];
+    }
 }
